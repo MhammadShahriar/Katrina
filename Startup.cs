b84d23edfc7791bfca18db8f6306eab8258f6af7
MhammadShahriar <md_shahriar@yahoo.com>
2022-12-04 17:18:08 +0600

    jjjjjjj

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppTestForGitHub009.Startup))]
+namespace WebAppTestForGitHub009
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
